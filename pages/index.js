b2e4d9fb2454c27d510df6ed3f4b1bd01d37c960
lithobraking <34843669+lithobraking@users.noreply.github.com>
2021-10-22 20:46:35 -0700

    home page products render as grid; added link function

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
 import Head from 'next/head';
-// import Image from 'next/image';
+import Link from 'next/link';
+import Image from 'next/image';
 // import styles from '../styles/Home.module.css';
 import products from '../products.json';
 import { Card, Container, Row } from 'react-bootstrap';
@@ -13,16 +14,19 @@ export default function Home() {
       </Head>
 
       <Container>
-        {products.map((product) => (
-          <Row>
-            <Card style={{ width: '18rem' }}>
-              <Card.Body>
-                <Card.Title>{product.name}</Card.Title>
-                <Card.Text>{product.meta_description}</Card.Text>
-              </Card.Body>
-            </Card>
-          </Row>
-        ))}
+        <Row xs={1} md={2} className="g-4">
+          {products.map((product) => (
+            <Link href={`/products/${product.slug}`}>
+              <Card className='m-2' style={{ width: '18rem' }}>
+                <Card.Body>
+                  <Card.Title>{product.name}</Card.Title>
+                  <Card.Subtitle className='mb-2 text-muted'>${product.price}</Card.Subtitle>
+                  <Card.Text>{product.meta_description}</Card.Text>
+                </Card.Body>
+              </Card>
+            </Link>
+          ))}
+        </Row>
       </Container>
     </div>
   )
